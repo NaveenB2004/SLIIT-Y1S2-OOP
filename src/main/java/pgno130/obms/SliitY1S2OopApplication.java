54a5c3027ab932e2f1d4c450f8681e2713599d64
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2025-05-05 13:53:53 +0530

    Initialize Project

--- src/main/java/pgno130/obms/SliitY1S2OopApplication.java
+++ src/main/java/pgno130/obms/SliitY1S2OopApplication.java
@@ -2,18 +2,10 @@ package pgno130.obms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class SliitY1S2OopApplication extends SpringBootServletInitializer {
+public class SliitY1S2OopApplication {
     public static void main(String[] args) {
         SpringApplication.run(SliitY1S2OopApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SliitY1S2OopApplication.class);
     }
 }
